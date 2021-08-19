0b816b94913af59d312b30e6a75020e68aa4333a
snedzin <snedzin@gmail.com>
2021-08-19 17:01:16 +0200

    MySql Connect pushed out

--- settings.php
+++ settings.php
@@ -5,3 +5,7 @@
 	$password = 'dhjnvytyjub'; //пароль, по умолчанию пустой
 	$db_name = 'zacmpvji_trytix'; //имя базы данных
 
+
+//Соединяемся с базой данных используя наши доступы:
+    $link = mysqli_connect($host, $user, $password, $db_name)
+    or die('Не удалось соединиться: ' . mysql_error());
