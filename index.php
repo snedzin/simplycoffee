81ffa96d4aa181dd4ef51c4fde42e3c069f714a0
snedzin <snedzin@gmail.com>
2021-08-19 16:48:27 +0200

    Settings puted out

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 //Устанавливаем доступы к базе данных:
-	$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-	$user = 'zacmpvji_trytix'; //имя пользователя, по умолчанию это root
-	$password = 'dhjnvytyjub'; //пароль, по умолчанию пустой
-	$db_name = 'zacmpvji_trytix'; //имя базы данных
+include 'settings.php';
 
 //Соединяемся с базой данных используя наши доступы:
 	$link = mysqli_connect($host, $user, $password, $db_name)
---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+//Устанавливаем доступы к базе данных:
+	$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+	$user = 'zacmpvji_trytix'; //имя пользователя, по умолчанию это root
+	$password = 'dhjnvytyjub'; //пароль, по умолчанию пустой
+	$db_name = 'zacmpvji_trytix'; //имя базы данных
+
