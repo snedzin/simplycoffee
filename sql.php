06ea7336dff509ef932731aad532fc2913cc4e82
snedzin <snedzin@gmail.com>
2021-08-20 11:09:59 +0200

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,14 +1,7 @@
 <?php 
 
 //Устанавливаем доступы к базе данных:
-	$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-	$user = 'zacmpvji_trytix'; //имя пользователя, по умолчанию это root
-	$password = 'dhjnvytyjub'; //пароль, по умолчанию пустой
-	$db_name = 'zacmpvji_trytix'; //имя базы данных
-
-//Соединяемся с базой данных используя наши доступы:
-	$link = mysqli_connect($host, $user, $password, $db_name)
-	or die('Не удалось соединиться: ' . mysql_error());
+include 'settings.php';
 
 	function query($query) 
 	{
